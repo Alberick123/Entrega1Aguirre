8fa22b4af6e60b9814df7ca4f9708038a263c205
Alan1590 <awg.1590@gmail.com>
2021-12-15 23:04:42 -0300

    Agregado de form.py, agregado de registro en admin, creacion de formulario de estadio

--- ProyectoClub/AppClub/admin.py
+++ ProyectoClub/AppClub/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Jugador, Disciplina, Estadio
 
-# Register your models here.
+admin.site.register(Jugador)
+admin.site.register(Disciplina)
+admin.site.register(Estadio)
